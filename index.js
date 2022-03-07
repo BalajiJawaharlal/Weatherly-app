ef17e7f97dc28b41a52c57b4c1cce3de68906014
Balaji J <83724643+BalajiJawaharlal@users.noreply.github.com>
2022-03-07 20:22:05 +0530

    Updated file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const request=require('request')
 const { response } = require('express')
 
 const app=express()
-<<<<<<< HEAD
+
 const apikey='078ef7056df51407ac1e2c7b57f727ea'
 =======
 
@@ -21,15 +21,7 @@ app.set('view engine','ejs')
 app.get('/',function(req,res){
     res.render('index',{weather:null,error:null})
 })
-/**
- * open api from site -> openweathermap.org
- * generate your own api key in site by logging in and delete tha api after your completion of your project.
- * It's free of cost
-<<<<<<< HEAD
- * openweathermap.org -> jbmersal@gmail.com  password -> bAl$3ji0
-=======
->>>>>>> 6b843482529d14a73a9971585d7694d71b00ceae
- */
+
 app.post('/',function(req,res){
     let city=req.body.city
     let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}`;
@@ -58,41 +50,3 @@ app.post('/',function(req,res){
 app.listen(3000,function(){
     console.log("Weatherly app listening on port 3000")
 });
-
-
-
-
-
-
-
-
-
-
-
-//This should be commented only
-/*
-app.post('/',function(req,res){
-    let city=req.body.city
-    let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}`;
-    console.log(req.body.city);
-    request(url,function(err,response,body){
-        if(err){
-            res.render("index",{weather:null,error:"Error, Please enter again"})
-        }
-        else{
-            let weather=JSON.parse(body);
-            if(weather.main==undefined){
-                res.render("index",{
-                    weather:null,
-                    error:"Error, Please enter again",
-                });
-            }
-            else{
-                let weathertext=`It's ${weather.main.temp} degree Celsius with ${weather.weather[0].main} in ${weather.name}!!`;
-                res.render("index",{weather:weathertext, error:null});
-                console.log("body:",body);
-            }
-        }
-    });
-});
-*/
